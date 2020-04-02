e66d50e428c71d0231d019b7c48cfa55018c3310
misaelalamillo <misaelalamillo@gmail.com>
2020-04-02 02:56:17 -0500

    Updates

---
+++ precache-manifest.31cd7f4e3fc9713802819fe2219e6d9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50eaee109b20c19c6601b4ad669179b7",
+    "url": "/app-employee-directory/index.html"
+  },
+  {
+    "revision": "5e45fc6ef704f6c88a84",
+    "url": "/app-employee-directory/static/js/2.30068739.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-employee-directory/static/js/2.30068739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "889d75222e2a5d98d2f0",
+    "url": "/app-employee-directory/static/js/main.774c94c4.chunk.js"
+  },
+  {
+    "revision": "0d287790ee6b1805febc",
+    "url": "/app-employee-directory/static/js/runtime-main.e09d7593.js"
+  }
+]);
