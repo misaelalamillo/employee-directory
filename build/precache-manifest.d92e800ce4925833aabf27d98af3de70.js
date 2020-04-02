c9662446e221190589e24caf1861691d6e9e00a3
misaelalamillo <misaelalamillo@gmail.com>
2020-04-02 10:19:36 -0500

    added cdn from bootstrap

---
+++ build/precache-manifest.d92e800ce4925833aabf27d98af3de70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c593cbe856897eb13bd63499755e24",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "5e45fc6ef704f6c88a84",
+    "url": "/employee-directory/static/js/2.30068739.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.30068739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "889d75222e2a5d98d2f0",
+    "url": "/employee-directory/static/js/main.774c94c4.chunk.js"
+  },
+  {
+    "revision": "77e942ecd2659d1f60eb",
+    "url": "/employee-directory/static/js/runtime-main.a9f4c25c.js"
+  }
+]);
